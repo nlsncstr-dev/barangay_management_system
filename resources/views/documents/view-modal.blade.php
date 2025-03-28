f838e55d59681ecc364b6da321e61039c63d6455
Nelson <ncastro@htechcorp.net>
2025-03-29 07:06:06 +0800

    added a button

--- resources/views/documents/view-modal.blade.php
+++ resources/views/documents/view-modal.blade.php
@@ -1,9 +1,9 @@
 <div style="justify-content: center; display: flex; flex-direction: column; align-items: center;">
     {{-- Document Viewer --}}
-    <iframe src="{{ $url }}" style="height: 100vh; width: 70%; border: none; overflow: hidden;"></iframe>
+    <iframe src="{{ $url }}" style="height: 50vh; width: 70%; border: none; overflow: hidden;"></iframe>
 
     {{-- Download Button --}}
-    <a href="{{ $url }}" download style="margin-top: 15px; padding: 10px 20px; background-color: #2563eb; color: white; border-radius: 6px; text-decoration: none; display: inline-block;">
+    <a href="{{ $url }}" download style="margin-top: 50px; padding: 10px 20px; background-color: #00fa9a; color: black; border-radius: 6px; text-decoration: none; display: inline-block; font-family: 'Roboto', sans-serif;">
         Download Document
     </a>
 </div>
