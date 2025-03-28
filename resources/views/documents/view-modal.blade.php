ec4b4bccc243b2069ce9b8ac6a5b81c0f31e066c
Nelson <ncastro@htechcorp.net>
2025-03-29 06:52:31 +0800

    added and fix view and download button on certificate request

--- resources/views/documents/view-modal.blade.php
+++ resources/views/documents/view-modal.blade.php
@@ -1,1 +1,9 @@
-<iframe src="{{ $url }}" style="height: 70vh;"></iframe>
+<div style="justify-content: center; display: flex; flex-direction: column; align-items: center;">
+    {{-- Document Viewer --}}
+    <iframe src="{{ $url }}" style="height: 100vh; width: 70%; border: none; overflow: hidden;"></iframe>
+
+    {{-- Download Button --}}
+    <a href="{{ $url }}" download style="margin-top: 15px; padding: 10px 20px; background-color: #2563eb; color: white; border-radius: 6px; text-decoration: none; display: inline-block;">
+        Download Document
+    </a>
+</div>
