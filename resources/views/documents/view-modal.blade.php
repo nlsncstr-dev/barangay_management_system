fa3d7c455073ed59f927773afddd4cf7989831c6
Nelson <ncastro@htechcorp.net>
2025-03-29 07:11:12 +0800

    updated the bg color

--- resources/views/documents/view-modal.blade.php
+++ resources/views/documents/view-modal.blade.php
@@ -3,7 +3,13 @@
     <iframe src="{{ $url }}" style="height: 50vh; width: 70%; border: none; overflow: hidden;"></iframe>
 
     {{-- Download Button --}}
-    <a href="{{ $url }}" download style="margin-top: 50px; padding: 10px 20px; background-color: #00fa9a; color: black; border-radius: 6px; text-decoration: none; display: inline-block; font-family: 'Roboto', sans-serif;">
+    <a 
+        href="{{ $url }}" 
+        download 
+        style="margin-top: 50px; padding: 10px 20px; background-color: #00fa9a; color: black; border-radius: 6px; text-decoration: none; display: inline-block; font-family: 'Roboto', sans-serif;"
+        onmouseover="this.style.backgroundColor='#3eb489'; this.style.color='white';"
+        onmouseout="this.style.backgroundColor='#00fa9a'; this.style.color='black';"
+    >
         Download Document
     </a>
 </div>
